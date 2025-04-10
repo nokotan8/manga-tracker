8830de3d5c43497b88e7a27cab698cecda587500
fancywhale23 <justintaoau@gmail.com>
2025-04-10 16:14:00 +1000

    moved backend to separate repo

--- backend/src/manga_db_init.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists Users (
-    client_id   char(19), 
-    username    varchar(20),
-    primary key (client_id),
-    check (client_id regexp '^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$')
-);
