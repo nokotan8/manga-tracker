8cfc727935187fc57b0e5d776c3ebc534cfdb0b0
fancywhale23 <justintaoau@gmail.com>
2025-04-10 22:07:32 +1000

    changed server port

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	server: {
+    host: '127.0.0.1',
+    port: 9291
+  }
 });
